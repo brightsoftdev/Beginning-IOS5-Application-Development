a9aaeb038809efacd1ce59fb6ee9e218b3d5bc00
youthcode <youthcode@163.com>
2012-10-18 22:49:16 +0800

    charp1 finished

---
+++ C01/01-begging/Begging/Begging/WYPViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYPViewController.h
+//  Begging
+//
+//  Created by WYP on 12-10-18.
+//  Copyright (c) 2012年 WYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYPViewController : UIViewController
+
+@end
---
+++ C01/01-begging/Begging/Begging/WYPViewController.m
@@ -0,0 +1,29 @@
+//
+//  WYPViewController.m
+//  Begging
+//
+//  Created by WYP on 12-10-18.
+//  Copyright (c) 2012年 WYP. All rights reserved.
+//
+
+#import "WYPViewController.h"
+
+@interface WYPViewController ()
+
+@end
+
+@implementation WYPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
